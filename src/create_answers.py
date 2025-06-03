1b8bb5dcb8c1476c0a1dd45ae6b6e3ad493730cd
Louis Müller <louis-mue@web.de>
2025-06-03 16:37:15 +0200

    Refactor create_answers.py: add tqdm and restructure code

--- src/create_answers.py
+++ src/create_answers.py
@@ -1,14 +1,10 @@
 import json
 import os
-from model import get_model, GeminiModel
 
-from google import genai
+from tqdm import tqdm
+from model import get_model
 
-from huggingface_hub import login
-
-from aae_translation import add_aae_to_df
 from utils import (
-    get_df_from_file,
     load_config,
     parse_args,
 )
@@ -17,30 +13,29 @@ model_name = "gemini-1.5-flash"
 
 args = parse_args()
 config = load_config(args.config_path)
-login(token=config["huggingface_hub_token"])
 
-gemini = GeminiModel(
-    model_name_or_path=model_name,
-    api_key=config["gemini_key"],
+model = get_model(
+    model_name_or_path=args.answer_generation_model_name_or_path,
+    config=config,
 )
 
 # generate answers without AAE
 with open("data/chen-et-al/data_with_aae_gpt4-1.json", "r") as f:
     data = [json.loads(line) for line in f.readlines()]
 
-for entry in data:
+for entry in tqdm(data):
     generated_data = {}
+    # copy original data 
+    generated_data["answers_gpt"] = entry["answers"]
+    for key in [key for key in entry.keys() if key not in ["answers"]]:
+        generated_data[key] = entry[key]
+
     prompt = entry["prompt"]
-    text = gemini.prompt(
+    text = model.generate_answers(
         message=prompt,
         num_generations=2,
     )
-    generated_data["question_id"] = entry["question_id"]
-    generated_data["question"] = entry["question"]
-    generated_data["prompt"] = prompt
-    generated_data["temperature"] = entry["temperature"]
-    generated_data["model_id"] = entry["model_id"]
-    generated_data["level"] = entry["level"]
+
     generated_data["answers_gemini"] = {
         "answer1": {
             "answer": text[0],
@@ -51,7 +46,7 @@ for entry in data:
             "answer_id": "NA",
         },
     }
-    generated_data["answers_gpt"] = entry["answers"]
+    
     with open(
         os.path.join(
             os.path.dirname(args.data_path),
@@ -60,17 +55,3 @@ for entry in data:
         "a",
     ) as f:
         f.write(json.dumps(generated_data) + "\n")
-
-# data_df = get_df_from_file(args.data_path)
-# data_directory = os.path.dirname(args.data_path)
-# if not os.path.exists(f"{data_directory}/data_with_aae_{model_name}.json"):
-#     data_df = add_aae_to_df(data_df, prompt_gen_model)
-#     data_df.to_json(
-#         f"{data_directory}/data_with_aae_{model_name}.json",
-#         lines=True,
-#         orient="records",
-#     )
-# else:
-#     data_df = get_df_from_file(
-#         os.path.join(data_directory, f"data_with_aae_{model_name}.json")
-#     )
