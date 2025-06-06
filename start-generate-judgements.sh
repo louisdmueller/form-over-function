77579df682b9e74a55297f732a8595df3fe93137
Louis Müller <lmueller@cl.uni-heidelberg.de>
2025-06-06 23:45:51 +0200

    Add generate-judgements slurm script

---
+++ start-generate-judgements.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --partition=dev_gpu_h100
+#SBATCH --job-name=LLM-Judge-Bias
+#SBATCH --output=%j.out
+#SBATCH --error=%j.err
+#SBATCH --time=00:30:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:2
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user="cluster-notifications.7fo8i@simplelogin.com"
+set -e
+
+echo Time is `date +"%H:%M %d-%m-%y"`
+
+export PYTHONPATH=${PWD}/src/
+
+## we have to decide which cuda version to use
+# module load devel/cuda/11.7
+module load devel/cuda/12.8
+
+# currently we use python 3.12.x
+module load devel/python/3.12.3-gnu-11.4
+
+if [ ! -d "venv" ]; then
+    echo "Creating virtual environment..."
+    python -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+else
+    source venv/bin/activate
+fi
+
+# python src/compare_model_answers.py
+bash weak_vs_strong_model.sh
