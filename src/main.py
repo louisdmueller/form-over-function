1b0871c779ab5c1306b333e3f352e1a34bad4723
Tetojiji <steinar.graessel@gmx.net>
2025-06-07 17:09:45 +0200

    Revert "remove login (already handled in get_model)"
    This reverts commit 5763d01adc66970d7b2d082042ca4d2da05997c5.

--- src/main.py
+++ src/main.py
@@ -20,6 +20,9 @@ def main() -> None:
     args = parse_args()
     config = load_config(args.config_path)
 
+    # access to llama models is restricted
+    login(token=config["huggingface_hub_token"])
+
     judge_model = get_model(
         model_name_or_path=args.judge_model_name_or_path,
         config=config,
