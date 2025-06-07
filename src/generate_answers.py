c6e60c0df037f97a74a04edb55e4d3da5939c896
Louis Müller <louis-mue@web.de>
2025-06-07 21:50:59 +0200

    Refactor output file handling to remove organization name in case of huggingface models in the output file name and lint file

--- src/generate_answers.py
+++ src/generate_answers.py
@@ -3,31 +3,39 @@ This script generates two files with answers to the prompts in the data file.
  - The first file contains the answers in Standard American English (SAE),
  - The second file (if AAE translation is requested) contains the answers translated to African American English (AAE)
 """
+
 import json
 import os
 from tqdm import tqdm
 
 from model import get_model
-from utils import (
-    load_config,
-    parse_args,
-    random_id
-)
+from utils import load_config, parse_args, random_id
 from aae_translation import add_aae_to_df
 
 args = parse_args()
 config = load_config(args.config_path)
 
 if os.path.exists(args.output_path):
-    print(f"Output file {args.output_path} already exists. Exiting to avoid overwriting.")
+    print(
+        f"Output file {args.output_path} already exists. Exiting to avoid overwriting."
+    )
     exit(1)
 
-if "/" in args.output_path:
-    if args.answer_generation_model_name_or_path in args.output_path:
-        args.output_path = args.answer_generation_model_name_or_path.split("/")[-1]
-    else:
-        # create directory if it does not exist
-        os.makedirs(os.path.dirname(args.output_path), exist_ok=True)
+if (
+    "/" in args.answer_generation_model_name_or_path
+    and args.answer_generation_model_name_or_path in args.output_path
+):
+    # If the model name contains a slash, it is a Hugging Face model
+    # but slash is also used in the path
+    # Since the name is often used in the output file name,
+    # we replace the slash with an underscore to avoid issues
+    model_name = args.answer_generation_model_name_or_path.split("/")[-1]
+    args.output_path = args.output_path.replace(
+        args.answer_generation_model_name_or_path, model_name
+    )
+
+if not os.path.exists(os.path.dirname(args.output_path)):
+    os.makedirs(os.path.dirname(args.output_path), exist_ok=True)
 
 answer_generation_model = get_model(
     model_name_or_path=args.answer_generation_model_name_or_path,
@@ -53,7 +61,7 @@ for entry in tqdm(data, desc=desc):
     prompt = entry["prompt"]
     responses = answer_generation_model.generate(
         system_prompts=[""],
-        input_texts = [prompt],
+        input_texts=[prompt],
         max_output_tokens=len(prompt) + 50,
         num_generations=2,
     )
@@ -62,7 +70,6 @@ for entry in tqdm(data, desc=desc):
         # generate returns a list in a list
         # so we need to extract the first element
         responses = responses[0]
-
 
     generated_data["answers"] = {
         "answer1": {
@@ -86,6 +93,7 @@ for entry in tqdm(data, desc=desc):
     if args.aae:
         # If AAE translation is requested, add AAE translations to the answers
         import pandas as pd
+
         df = pd.DataFrame([generated_data])
         df = add_aae_to_df(df, prompt_gen_model)
 
