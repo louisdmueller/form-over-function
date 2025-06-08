2efa8888a4e6ed3eb19bbf552ee5f83644baf717
Louis Müller <lmueller@cl.uni-heidelberg.de>
2025-06-08 11:18:37 +0200

    Update current state of slurm scripts

--- generate_data.sh
+++ generate_data.sh
@@ -2,12 +2,14 @@ set -e # Exit on error
 set -u # Treat unset variables as an error
 
 # answer_generation_model_name_or_path="gemini-1.5-flash"  # model that generates answers to the questions
-answer_generation_model_name_or_path="meta-llama/Llama-3.1-8B-Instruct"
+# answer_generation_model_name_or_path="meta-llama/Llama-3.1-8B-Instruct"
+# answer_generation_model_name_or_path="openai-community/gpt2"
+answer_generation_model_name_or_path="EleutherAI/gpt-neo-1.3B"
 prompt_model_name_or_path="gemini-1.5-flash" # model that converts answers to aae answers # only used if --aae is set
 
 python src/generate_answers.py \
     --answer_generation_model_name_or_path "$answer_generation_model_name_or_path" \
     --prompt_model_name_or_path "$prompt_model_name_or_path" \
     --output_path "data/$answer_generation_model_name_or_path-answers.json" \
-    --aae
+#     --aae
     # TODO: implement input_path so already generated answers can be translated to aae
--- generate_judgements.sh
+++ generate_judgements.sh
@@ -10,8 +10,10 @@ set -u # Treat unset variables as an error
 
 answer_generation_model_name_or_path="gemini-1.5-flash-8b"
 better_model_name="gemini-1.5-pro-8b"
-# judge_model_name="meta-llama/Llama-3.3-70B-Instruct"
-judge_model_name="RandomAnswer"
+#judge_model_name="meta-llama/Llama-3.3-70B-Instruct"
+#judge_model_name="RandomAnswer"
+judge_model_name="meta-llama/Llama-3.1-8B-Instruct"
+
 
 ### 0. Generate worse answers
 # python src/generate_answers.py \
@@ -22,9 +24,10 @@ judge_model_name="RandomAnswer"
 ### 1. Compare answers from the worse model to the answers from the better model
 python src/compare_model_answers_batched.py \
     --judge_model_name_or_path "$judge_model_name" \
-    --data_1_path "data/gpt-4.json" \
-    --data_2_path "data/gemini-1.5-flash.json" \
-    --end-index 0.5
+    --data_1_path "data/gpt-4-original-answers.json" \
+    --data_2_path "data/gemini-1.5-flash-answers.json" \
+    --start_index 0.5
+#    --end_index 0.5
     # --question_switching # TODO
     # --prompt_switchging # TODO
 
