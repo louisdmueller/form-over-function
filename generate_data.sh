c5317e36a6a985a2d50462286c8fe801e942d89b
Tetojiji <steinar.graessel@gmx.net>
2025-06-09 15:37:38 +0200

    Update usage documentation and use positional arguments

--- generate_data.sh
+++ generate_data.sh
@@ -1,17 +1,33 @@
+#!/bin/bash
 set -e # Exit on error
 set -u # Treat unset variables as an error
 
-# answer_generation_model_name_or_path="gemini-1.5-flash"  # model that generates answers to the questions
-# answer_generation_model_name_or_path="meta-llama/Llama-3.1-8B-Instruct"
-# answer_generation_model_name_or_path="openai-community/gpt2"
-# answer_generation_model_name_or_path="EleutherAI/gpt-neo-1.3B"
-answer_generation_model_name_or_path="EleutherAI/gpt-neox-20b"
-# answer_generation_model_name_or_path="huggyllama/llama-13b"
-prompt_model_name_or_path="gpt-4o-mini" # model that converts answers to aae answers # only used if --aae is set
+: ' 
+Usage: ./generate_data.sh <answer_generation_model> [aae_conversion_model]
+Arguments:
+    answer_generation_model (required) Name / path of model used to generate answers
+                                Example models:
+                                - gemini-1.5-flash
+                                - meta-llama/Llama-3.1-8B-Instruct
+                                - openai-community/gpt2
+                                - EleutherAI/gpt-neo-1.3B
+                                - EleutherAI/gpt-neox-20b
 
-python src/generate_answers.py \
-    --answer_generation_model_name_or_path "$answer_generation_model_name_or_path" \
-    --prompt_model_name_or_path "$prompt_model_name_or_path" \
-    --output_path "data/$answer_generation_model_name_or_path-answers.json" \
-#     --aae
-    # TODO: implement input_path so already generated answers can be translated to aae
+    aae_conversion_model    (optional) Converts SAE answers to AAE
+'
+
+if [[ $# -ge 2 ]]; then
+    echo "Converting SAE answers to AAE, since conversion model was given."
+    python src/generate_answers.py \
+        --answer_generation_model_name_or_path "$1" \
+        --prompt_model_name_or_path "$2" \
+        --output_path "data/$1-answers.json" \
+        --aae
+else
+   echo "Running generation without converting of answers to AAE."
+    python src/generate_answers.py \
+        --answer_generation_model_name_or_path "$1" \
+        --output_path "data/$1-answers.json"
+fi
+
+# TODO: implement input_path so already generated answers can be translated to aae
