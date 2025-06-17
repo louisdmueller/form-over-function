4f8ee32f4bcafb510c04aeff3619213adcd108c5
Louis Müller <lmueller@cl.uni-heidelberg.de>
2025-06-17 22:48:20 +0200

    Add automatic xlsx file creation to slurm script; Add cache_dir and force_download to config

--- start-generate-judgements.sh
+++ start-generate-judgements.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --partition=dev_cpu
-#SBATCH --job-name=GPT4.1_AAE-vs-GPT-Neox-0.5
+#SBATCH --partition=dev_gpu_h100
+#SBATCH --job-name=Mistral-Large-Instruct-2411
 #SBATCH --output=%j-%x.out
 #SBATCH --error=%j-%x.err
-#SBATCH --time=00:05:00
+#SBATCH --time=00:30:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=32G
-## SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user="cluster-notifications.7fo8i@simplelogin.com"
 set -e
@@ -33,25 +33,33 @@ else
     source venv/bin/activate
 fi
 
-# Output directory is the name of the job
-output_dir="data/${SLURM_JOB_NAME}"
-mkdir -p "$output_dir"
-
+data_path="data/"
 
 judge_model_name="meta-llama/Llama-3.3-70B-Instruct"
-# judge_model_name="RandomAnswer"
-judge_model_name="meta-llama/Llama-3.1-8B-Instruct"
+# judge_model_name="meta-llama/Llama-3.1-8B-Instruct"
 # judge_model_name="Qwen/Qwen2.5-72B-Instruct"
-judge_model_name="RandomAnswer"
+# judge_model_name="mistralai/Mistral-7B-Instruct-v0.2"
+# judge_model_name="mistralai/Mistral-Large-Instruct-2411"
+
+model_1_file="gpt-4.1-answers_aae.json"
+model_2_file="Mistral-7B-Instruct-v0.2-answers.json"
+
+model_1=$(echo "$model_1_file" | sed -E 's/-answers(_[a-z]+)?\.json$/\1/')
+model_2=$(echo "$model_2_file" | sed -E 's/-answers(_[a-z]+)?\.json$/\1/')
+
+# Output directory is the combination of the models and the judge model
+experiment_name="$(basename "${judge_model_name}")---$model_1-vs-$model_2"
+output_dir="$data_path/$experiment_name"
+mkdir -p "$output_dir"
 
 ### Compare answers from the worse model to the answers from the better model
 python src/compare_model_answers_batched.py \
     --judge_model_name_or_path "$judge_model_name" \
-    --data_1_path "data/gpt-4.1-answers_aae.json" \
-    --data_2_path "data/gpt-neox-20b-answers-temperature-0.5.json" \
+    --data_1_path "data/$model_1_file" \
+    --data_2_path "data/$model_2_file" \
     --output_path "$output_dir" \
     --start_index "auto" \
-    --step_size 71 # optional, default is 32
+    --step_size 71 # optional, default is 64
     # --question_switching # uncomment to switch questions between e.g. AAE and SAE style (depends if questions in files differ)
     # --introductionary_beginning # uncomment to add an introductionary beginning to the prompt e.g. "Hi there, I am kind of stuck on this question..."
     # --prompt_switching # TODO
@@ -59,10 +67,15 @@ python src/compare_model_answers_batched.py \
 
 # This checks whether all answers have been generated
 # If script returns 0, we will continue with merging the results
-if python src/check_if_all_data_processed.py --data_1_path "data/gpt-4.1-answers_aae.json" --input_dir "$output_dir/"; then
+if python src/check_if_all_data_processed.py --data_1_path "data/gpt-4.1-answers.json" --input_dir "$output_dir/"; then
     echo "All answers have been generated. Proceeding to merge results."
     python src/merge_json_files.py \
         --merge_path "$output_dir"
+    
+    echo "Converting merged JSON to XLSX..."
+    python src/export_to_xlsx.py \
+        --json_path "$output_dir/merged_data.json" \
+        --xlsx_path "$output_dir/$experiment_name.xlsx"
 else
     echo "Not all answers have been generated. Exiting."
 fi
