e6fc1a81d55d39f74b693203f54b3fb03482b22b
Steinar <graessel@cl.uni-heidelberg.de>
2025-06-17 23:09:55 +0200

    add command line args for slurm generate_data script (still works the same when used without args)

--- start-generate-data.sh
+++ start-generate-data.sh
@@ -11,7 +11,8 @@
 #SBATCH --gres=gpu:2
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user="cluster-notifications.7fo8i@simplelogin.com"
-set -e
+
+set -e # Exit on error
 
 echo Time is `date +"%H:%M %d-%m-%y"`
 
@@ -33,17 +34,31 @@ else
     source venv/bin/activate
 fi
 
-# answer_generation_model_name_or_path="gemini-1.5-flash"  # model that generates answers to the questions
-# answer_generation_model_name_or_path="meta-llama/Llama-3.1-8B-Instruct"
-# answer_generation_model_name_or_path="openai-community/gpt2"
-# answer_generation_model_name_or_path="EleutherAI/gpt-neo-1.3B"
-answer_generation_model_name_or_path="EleutherAI/gpt-neox-20b"
-# answer_generation_model_name_or_path="huggyllama/llama-13b"
-prompt_model_name_or_path="gpt-4o-mini" # model that converts answers to aae answers # only used if --aae is set
+answer_generation_model="gemini-1.5-flash"
+aae_conversion_model=""
 
-python src/generate_answers.py \
-    --answer_generation_model_name_or_path "$answer_generation_model_name_or_path" \
-    --prompt_model_name_or_path "$prompt_model_name_or_path" \
-    --output_path "data/$answer_generation_model_name_or_path-answers.json" \
-    # --aae
-    # TODO: implement input_path so already generated answers can be translated to aae
+if [[ $# -ge 1 ]]; then
+    answer_generation_model=$1
+fi
+
+if [[ $# -ge 2 ]]; then
+    aae_conversion_model=$2
+fi
+
+if [[ -n "$aae_conversion_model" ]]; then
+    echo "Converting SAE answers to AAE, since conversion model was given.
+    Chosen answer generation model: $answer_generation_model
+    Chosen AAE conversion model: $aae_conversion_model"
+    python src/generate_answers.py \
+        --answer_generation_model_name_or_path "$answer_generation_model" \
+        --prompt_model_name_or_path "$aae_conversion_model" \
+        --output_path "data/$answer_generation_model-answers.json" \
+        --aae
+        
+else
+   echo "Running generation without converting of answers to AAE."
+    echo "Chosen answer generation model: $answer_generation_model"
+    python src/generate_answers.py \
+        --answer_generation_model_name_or_path $answer_generation_model \
+        --output_path "data/$answer_generation_model-answers.json"
+fi
