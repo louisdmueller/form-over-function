c138914af80d440420b49340c5968882696b6576
Arcumenn <steinar.graessel@gmx.net>
2025-06-18 02:22:46 +0200

    updated plot file

--- src/plotASR.py
+++ src/plotASR.py
@@ -1,9 +1,9 @@
 import plotly.graph_objects as go
 
-categories = ['Llama3.1-8B-Instruct', 'Qwen2-0.5B-Instruct', 'Qwen2.5-3B-Instruct', 'Qwen2-7B-Instruct']
-group1 = [0.85, None, 0.7091, None]
-group2 = [0.8148, None, 0.7797, None]
-group3 = [0.6098, 0.2889, 0.6724, 0.6176]
+categories = ['Qwen2-0.5B-Instruct', 'Qwen2.5-3B-Instruct', 'Mistral-7B-Instruct-v0.2', 'Llama3.1-8B-Instruct']
+group1 = [0.4812, 0.7091, 0.8197, 0.85]
+group2 = [0.2963, 0.7797, 0.413, 0.8148]
+group3 = [0.2889, 0.6724, 0.4359, 0.6098]
 
 fig = go.Figure(data=[
     go.Bar(name='Llama-3.3-70B-Instruct', x=categories, y=group1),
@@ -13,8 +13,8 @@ fig = go.Figure(data=[
 
 fig.update_layout(
     barmode='group',
-    title='ASR when attacking GPT-4.1'
-    xaxis_title='Weaker Models',
+    title='ASR when attacking GPT-4.1 by converting its stronger answers from SAE to AAE',
+    xaxis_title='Weaker Models, whose answers are not converted',
     yaxis_title='ASR (Attack Success Rate)',
     legend=dict(
         title='Judge Models'
