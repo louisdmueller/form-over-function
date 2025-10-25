9335d8d4638bcd7446819eb2cc36786c619b0238
Arcumenn <steinar.graessel@gmx.net>
2025-10-26 00:54:59 +0200

    refactor / format code for clarity/unity

--- src/calculate_readability_metrics.py
+++ src/calculate_readability_metrics.py
@@ -1,14 +1,17 @@
-from datasets import load_dataset
-from easse.sari import corpus_sari
-from bert_score import score
-from spacy.matcher import Matcher
-from typing import List
-from utils import read_file, write_file
 import json
+from typing import List
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 import spacy
 import textstat as ts
+from bert_score import score
+from datasets import load_dataset
+from easse.sari import corpus_sari
+from spacy.matcher import Matcher
+from statistics import mean
+
+from utils import read_file, write_file
 
 
 def calculate_readability_metrics(texts: List[str] | str, verbose: bool = True) -> dict:
@@ -57,7 +60,52 @@ def calculate_readability_metrics(texts: List[str] | str, verbose: bool = True) 
     return metrics
 
 
-def calculate_passive_ratio(texts: List[str]):
+def plot_readability_metrics(
+    readability_metrics: List[dict], output_dir: str = "data/readability_metrics"
+) -> None:
+    """
+    Save a boxplot for each readability metric; each plot compares the five
+    different texts: Input (advanced), simple prompt output, complex prompt
+    output, Basic English prompt output, and reference (elementary).
+
+    Args:
+      readability_metrics (tuple[dict, dict]): A tuple containing two
+        dictionaries with readability metrics for SAE and Basic English
+        answers, respectively. Each dictionary should be an output of
+        'calculate_readability_metrics'.
+      output_dir (str): Directory to save the plots.
+    """
+    # title = "Readability Comparison: SAE vs Basic English Answers --"
+    for metric in readability_metrics[0].keys():
+        # Set plot style
+        sns.set_theme(style="whitegrid")
+        # Create a boxplot
+        plt.figure(figsize=(10, 6))
+        sns.boxplot(
+            data=[
+                readability_metric[metric] for readability_metric in readability_metrics
+            ]
+        )
+        plt.xticks(
+            [0, 1, 2, 3, 4],
+            [
+                "Advanced Article (Input)",
+                "Simple Prompt Rewrite",
+                "Complex Prompt Rewrite",
+                "Basic English Prompt Rewrite",
+                "Elementary Article (Reference)",
+            ],
+        )
+        plt.xticks(rotation=15, ha="right")
+        plt.ylabel(metric.replace("_", " ").title())
+        # plt.title(f"{title} {metric.replace("_", " ").title()}")
+        # Save to file
+        plt.tight_layout()
+        plt.savefig(f"{output_dir}/{metric}.png")
+        plt.close()
+
+
+def calculate_passive_ratio(texts: str) -> List[float]:
     """
     Calculate the ratio of passive constructions per sentence in a list of
     texts.
@@ -74,16 +122,19 @@ def calculate_passive_ratio(texts: List[str]):
         doc = nlp(text)
         sents = list(doc.sents)
         passive_rule = [
-            {'DEP': 'nsubjpass'}, {'DEP': 'aux', 'OP': '*'},
-            {'DEP': 'auxpass'}, {'TAG': 'VBN'}]
-        matcher.add('Passive', [passive_rule])
+            {"DEP": "nsubjpass"},
+            {"DEP": "aux", "OP": "*"},
+            {"DEP": "auxpass"},
+            {"TAG": "VBN"},
+        ]
+        matcher.add("Passive", [passive_rule])
         matches = matcher(doc)
         passive_ratios.append(len(matches) / len(sents))
 
     return passive_ratios
 
 
-def load_onestopqa(reference: bool=True)-> List[List[str]]:
+def load_onestopqa(reference: bool = True) -> List[List[str]]:
     """
     Load articles from the OneStopQA dataset.
 
@@ -102,8 +153,7 @@ def load_onestopqa(reference: bool=True)-> List[List[str]]:
     count = 0
     title = dataset["train"][0]["title"]
     range_start = 6 if reference else 0
-    train_data = dataset["train"].select(
-        range(range_start, len(dataset["train"]), 9))
+    train_data = dataset["train"].select(range(range_start, len(dataset["train"]), 9))
 
     for paragraph in train_data:
         if paragraph["title"] == title:
@@ -116,14 +166,17 @@ def load_onestopqa(reference: bool=True)-> List[List[str]]:
 
     return texts
 
-def calculate_sari(path_to_system_output:str, paragraph_count:int=3) -> float:
+
+def calculate_sari_onestopQA(
+    path_to_system_output: str, paragraph_count: int = 3
+) -> float:
     """
     Calculate SARI for system output generated from the OneStopQA dataset.
-    
+
     Args:
       path_to_system_output (str): Path to a JSON file containing a list of
         system output articles (e.g. in 'data/readability_metrics').
-      paragraph_count (int): Number of paragraphs to consider from each 
+      paragraph_count (int): Number of paragraphs to consider from each
         article. Should match the number of paragraphs used for system output.
 
     Returns:
@@ -131,18 +184,21 @@ def calculate_sari(path_to_system_output:str, paragraph_count:int=3) -> float:
     """
     original_articles = load_onestopqa(reference=False)
     reference_articles = load_onestopqa(reference=True)
-    original_articles_truncated = [" ".join(
-        paragraph[:paragraph_count]) for paragraph in original_articles]
+    original_articles_truncated = [
+        " ".join(paragraph[:paragraph_count]) for paragraph in original_articles
+    ]
     # reference articles need to be list of lists
-    reference_articles_truncated = [[" ".join(
-        paragraph[:paragraph_count]) for paragraph in reference_articles]]
+    reference_articles_truncated = [
+        [" ".join(paragraph[:paragraph_count]) for paragraph in reference_articles]
+    ]
     with open(path_to_system_output, "r") as file:
         generated_articles = json.load(file)
 
     corpus_sari_sae = corpus_sari(
-        orig_sents=original_articles_truncated, 
-        sys_sents=generated_articles, 
-        refs_sents=reference_articles_truncated)
+        orig_sents=original_articles_truncated,
+        sys_sents=generated_articles,
+        refs_sents=reference_articles_truncated,
+    )
 
     return corpus_sari_sae
 
