94f67dca65bec4292f83607fb6630c1b7d2179eb
Louis Müller <louis-mue@web.de>
2026-01-06 12:16:37 +0100

    Add scripts for sending and retrieving batches

--- claude-retrieve.sh
+++ claude-retrieve.sh
@@ -1,4 +1,4 @@
 echo "Time is $(date +"%H:%M %d-%m-%y") ($(date +%s))"
 
 export ANTHROPIC_MODE=retrieve
-python src-new/main.py --tasks_file closed_models_tasks_files/tasks_Claude_Haiku_4.5.json
+python src-new/main.py --tasks_file closed_models_tasks_files/tasks_Claude_Sonnet_4.5.json
